917c972b07251f7b41b9c4295bc4f43370ff5569
Xavier Lambein <xavier@lambein.xyz>
2025-04-05 10:23:43 +0200

    Quit on CTRL+Q

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Update, quit_on_ctrl_q)
         .run();
+}
+
+fn quit_on_ctrl_q(keys: Res<ButtonInput<KeyCode>>, mut exit: EventWriter<AppExit>) {
+    if keys.any_pressed([KeyCode::ControlLeft, KeyCode::ControlRight])
+        && keys.just_pressed(KeyCode::KeyQ)
+    {
+        exit.send(AppExit::Success);
+    }
 }
